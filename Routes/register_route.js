bf8f63a769420958ca7510f02406cfd0cc7f2063
AKASH <akash.arthanur26@gmail.com>
2022-12-10 12:54:22 +0530

    login end point created

--- Routes/register_route.js
+++ Routes/register_route.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
 const { RegisterDB, PostDB } = require("../models/Schema");
+
+const secret = "MYAPIAUTH";
 
 const User_route = express.Router();
 User_route.get("/", (req, res) => {
@@ -46,9 +49,50 @@ User_route.post(
     }
   }
 );
-User_route.post("/login", async (req, res) => {
+//===================================================================================================================================================
+User_route.post("/login", body("email").isEmail(), async (req, res) => {
   try {
-  } catch (e) {}
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    const user = await RegisterDB.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Email not registered",
+      });
+    }
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (err) {
+        return res.status(500).json({
+          message: err.message,
+        });
+      }
+      if (result) {
+        const token = jwt.sign(
+          {
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            data: user._id,
+          },
+          secret
+        );
+        res.status(200).json({
+          status: "User login successfull",
+          token: token,
+        });
+      } else {
+        res.status(400).json({
+          status: "Login failed",
+          message: "Invalid credentials",
+        });
+      }
+    });
+  } catch (e) {
+    res.status(400).json({
+      message: e.message,
+    });
+  }
 });
 
 module.exports = User_route;
